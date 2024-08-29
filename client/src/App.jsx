b29953e4d3d6089ef617bb22f7d8b631a30738ef
Sohail Ali Khwazada <sohailali.khwazada2011@gmail.com>
2024-08-29 09:50:34 +0530

    starter-code

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { TempButtons } from "./pages/Supervisor/TempButtons";
+
+const App = () => {
+  return (
+    <div>
+      <TempButtons />
+    </div>
+  );
+};
+
+export default App;
