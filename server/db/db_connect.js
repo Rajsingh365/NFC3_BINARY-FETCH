230843773d2fa4bad32b856e9eaf173e31d455b8
Ashish Gupta <ashish279338@gmail.com>
2024-08-29 10:32:35 +0530

    db connect

--- server/db/db_connect.js
+++ server/db/db_connect.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
-
+import 'dotenv/config'
 export default async function connectDb() {
   try {
+    console.log(process.env.MONGO_URI)
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB");
   } catch (error) {
