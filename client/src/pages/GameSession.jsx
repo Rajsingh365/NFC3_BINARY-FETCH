2b1b5f0d0dcf0b96c86261e44a4ae1c4ce30587c
Dhruv Harish Agrahari <122349013+Dhruvagrahari25@users.noreply.github.com>
2024-08-30 00:19:19 +0530

    some changes, more like no changes

--- client/src/pages/GameSession.jsx
+++ client/src/pages/GameSession.jsx
@@ -1,16 +1,9 @@
 import React from 'react'
-import GameSessionImg from '../assets/images/GameSession/GameSessionImg.jpg';
 
 const GameSession = () => {
   return (
-    <div 
-      className='flex flex-col justify-center'
-    >
-      <img 
-        src={GameSessionImg}
-        alt="Characters Graphic" 
-        className='h-full w-auto object-fit'
-      />
+    <div>
+      Game Session
     </div>
   )
 }
